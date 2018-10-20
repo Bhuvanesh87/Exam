6eaa938662030237268373cfbafcc958f9a82921
Bhuvanesh87 <bhuvanesh.arveti@gmail.com>
2018-10-20 21:22:52 +0530

    first commit

---
+++ Q3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Exam
+{
+    class Person
+    {
+        public int age { set; get; }
+        public string name { set; get; }
+        public override string ToString()
+        {
+            return string.Format("Name : {0}  \nAge : {1}", name, age);
+        }
+    }
+    class Q3
+    {
+        static void Main(string[] args)
+        {
+            string JSONstring = File.ReadAllText(@"F:\\Bizruntime\\prac\\exam\\Exam\\Exam\\JSON.json");
+
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+             Person p1 = ser.Deserialize<Person>(JSONstring);
+             Console.WriteLine(p1);       
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ Q6.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Q6
+    {
+        static SqlConnection con;
+        static int id;
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                con = new SqlConnection("data source=.; database=Bhuvan; integrated security=SSPI");
+                con.Open();
+                while (true)
+                {
+                  lable1:
+                  Console.WriteLine("----------Welcome to Comany  management---------- ");
+                  Console.WriteLine("1. Enter 1 for show table items ");               
+                  Console.WriteLine("2. Enter 2 Exit ");                
+                  int acc_var;
+                
+                    acc_var = Int32.Parse(Console.ReadLine());
+                    switch (acc_var)
+                    {
+                        case 1:                            
+                            select_data();                            
+                            goto lable1;
+                        case 2:
+                            Console.WriteLine("Coming out of the database");
+                            return;
+                            
+
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Bye");
+                con.Close();
+
+                Console.ReadLine();
+            }
+            Console.Read();
+        }
+        public static void select_data()
+         {
+            Console.WriteLine("Enter the id to display the details");
+            id = Convert.ToInt32(Console.ReadLine());
+            SqlCommand cm = new SqlCommand("Select id,company,branch from Company where id="+id, con);
+            SqlDataReader sdr = cm.ExecuteReader();
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("ID          CompanyName                        Branch      ");
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            // Iterating Data  
+            while (sdr.Read())
+            {
+
+                Console.WriteLine(sdr["id"] + "              " + sdr["company"] + "                 " + sdr["branch"]); // Displaying Record  
+            }
+            Console.ReadLine();
+            Console.Clear();
+        }      
+
+    }
+}
